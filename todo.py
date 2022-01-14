aa760a1fd573b37bafd4ac4170e6b45220114c4c
Ufuk Orhan <ufuk.orhan@logsign.net>
2022-01-14 18:44:29 +0300

    feat: add todos endpoints

--- todo.py
+++ todo.py
@@ -1,12 +1,21 @@
+from crypt import methods
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import uuid
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+load_dotenv()
+
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY'] = 'secret' # It’s used by some middleware (extensions) that have to do with security (for now we have none)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/ufuk/Desktop/restfull-api/todo.db' # which is the database curl for our app
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("database_path") # which is the database curl for my app
 
 
 class User(db.Model):
@@ -49,7 +58,6 @@ def get_users():
 
 @app.route('/users/<id>/')
 def get_user(id):
-	print(id)
 	user = User.query.filter_by(public_id=id).first_or_404()
 	return {
 		'id': user.public_id, 'name': user.name, 
@@ -93,7 +101,7 @@ def update_user(id):
 	user = User.query.filter_by(public_id=id).first_or_404()
 	user.name=data['name']
 	if 'is admin' in data:
-		user.is_admin=data['admin']
+		user.is_admin=data['is admin']
 	db.session.commit()
 	return jsonify({
 		'id': user.public_id, 
@@ -105,6 +113,104 @@ def update_user(id):
 def delete_user(id):
 	user = User.query.filter_by(public_id=id).first_or_404()
 	db.session.delete(user)
+	db.session.commit()
+	return {
+		'success': 'Data deleted successfully'
+	}
+
+# Above part includes user endpoints
+# ----------------------------------
+# Upper part includes todo endpoints
+
+@app.route('/todos/')
+def get_todos():
+	return jsonify([
+		{ 
+			'id': todo.public_id, 'name': todo.name,
+			'owner': {
+				'name': todo.owner.name,
+				'email': todo.owner.email,
+				'public_id': todo.owner.public_id
+			}
+		} for todo in Todo.query.all()
+	])
+
+@app.route('/todos/<id>')
+def get_todo(id):
+	todo = Todo.query.filter_by(public_id=id).first_or_404()
+	return jsonify({ 
+			'id': todo.public_id, 'name': todo.name,
+			'owner': {
+				'name': todo.owner.name,
+				'email': todo.owner.email,
+				'public_id': todo.owner.public_id
+			}
+		})
+
+@app.route('/todos/', methods=['POST'])
+def create_todo():
+	data = request.get_json()
+	if not 'name' in data or not 'email' in data:
+		return jsonify({
+			'error': 'Bad Request',
+			'message': 'Name of todo or email of creator not given'
+		}), 400
+	if len(data['name']) < 4:
+		return jsonify({
+			'error': 'Bad Request',
+			'message': 'Name of todo contain minimum of 4 letters'
+		}), 400
+
+	user=User.query.filter_by(email=data['email']).first()
+	if not user:
+		return {
+			'error': 'Bad request',
+			'message': 'Invalid email, no user with that email'
+		}
+	is_completed = data.get('is completed', False)
+	todo = Todo(
+		name=data['name'], user_id=user.id,
+		is_completed=is_completed, public_id=str(uuid.uuid4())
+	)
+	db.session.add(todo)
+	db.session.commit()
+	return {
+		'id': todo.public_id, 'name': todo.name, 
+		'completed': todo.is_completed,
+		'owner': {
+			'name': todo.owner.name,
+			'email': todo.owner.email,
+			'is admin': todo.owner.is_admin 
+		} 
+	}, 201
+
+@app.route('/todos/<id>/', methods=['PUT'])
+def update_todo(id):
+	data = request.get_json()
+
+	if not 'name' in data or not 'completed' in data:
+		return {
+			'error': 'Bad Request',
+			'message': 'Name or completed fields need to be present'
+		}, 400
+	todo = Todo.query.filter_by(public_id=id).first_or_404()
+	todo.name=data.get('name', todo.name)
+	if 'completed' in data:
+		todo.is_completed=data['completed']
+	db.session.commit()
+	return {
+		'id': todo.public_id, 'name': todo.name, 
+		'is completed': todo.is_completed,
+		'owner': {
+			'name': todo.owner.name, 'email': todo.owner.email,
+			'is admin': todo.owner.is_admin 
+		} 
+	}, 201
+
+@app.route('/todos/<id>/', methods=['DELETE'] )
+def delete_todo(id):
+	todo = Todo.query.filter_by(public_id=id).first_or_404()
+	db.session.delete(todo)
 	db.session.commit()
 	return {
 		'success': 'Data deleted successfully'
